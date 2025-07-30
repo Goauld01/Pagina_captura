6537bc2a433ccda2a916fdb7322a233aef65e909
Fernando C Pereira <goauld@msn.com>
2025-07-30 19:43:47 -0300

    Adicionado a funcionalidade do popup.

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const botaos = document.querySelectorAll(".js-botao");
+
+botaos[0].addEventListener("click", () => {
+  document.body.classList.toggle("popup--aberto");
+});
+botaos[1].addEventListener("click", () => {
+  document.body.classList.toggle("popup--aberto");
+});
+botaos[2].addEventListener("click", () => {
+  document.body.classList.toggle("popup--aberto");
+});
+botaos[3].addEventListener("click", () => {
+  document.body.classList.toggle("popup--aberto");
+});
